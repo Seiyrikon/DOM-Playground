32827ce2a068f5765d0d87e2a214d52f86f5c30f
Seiyrikon <rickygalpo7777@gmail.com>
2023-11-02 12:02:38 +0800

    added a callback function on my ajax call for DomPlayground

--- script/ajax.js
+++ script/ajax.js
@@ -3,8 +3,14 @@ function loadDoc() {
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             document.getElementById("content").innerHTML = this.responseText;
+
+            // Call a function to execute the JavaScript code in the loaded content
+            initializeLoadedContent();
         }
     };
     xhttp.open("GET", "/template/DomPlayground.html", true);
     xhttp.send();
 }
+
+// Function to execute JavaScript code in the loaded content
+
